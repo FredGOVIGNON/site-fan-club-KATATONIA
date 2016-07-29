061511f9e15f23fd452baad78b739d65feee4e5b
FredGOVIGNON <fgovignon@gmail.com>
2016-07-29 10:17:30 +0200

    maj vendredi 29-07-16

--- src/AppBundle/Controller/DiscoController.php
+++ src/AppBundle/Controller/DiscoController.php
@@ -15,4 +15,27 @@ class DiscoController extends Controller
             
         ));
     }
+
+    public function voteAction(Request $request, $idalbum)
+    {
+     	$em=$this->getDoctrine()->getManager();
+      
+     	$user = $this->container->get('security.context')->getToken()->getUser();
+		$valuevote = 1;
+
+		$vote = new Vote;
+		$vote->setIduser($user->getId());
+		$vote->setIdalbum($idalbum);
+		$vote->setValue($vote->getValue() + $valuevote);
+
+		$em->persist($vote);
+		$em->flush;
+
+		$allVote = $em->getRepository('AppBundle:Vote')->findAll();
+
+		return $this->render('default/disco.html.twig', array(
+			'allVote' => $allVote,
+		));
+
+ 	} 
 }
